77eee8eb738e91e6801dc7a20c8f0b21ef616bd6
prabath17 <136706003+prabath17@users.noreply.github.com>
2025-01-26 18:39:37 +0530

    Add files via upload

---
+++ mysterious_no.py
@@ -0,0 +1,21 @@
+#here i import the package random
+#random.random() it generate random no like int , float, likewise
+#randint(startvalue,endvalue)  i want only integer give 
+# uniform(startvalue,endvalue)  i want only float give  
+#choice(list),and sample(list,num of sample case)
+#shuffle(list) to shuffle the list
+
+
+import random
+a=random.random() 
+b=random.random()
+'''if a>b:
+    print("a is greater")
+else:
+    print("b is greater")'''
+print("a == b: ",a == b)    # Equal to
+print("a != b:", a != b)  # Not equal to
+print("a < b:" ,a < b)    # Less than
+print("a > b:", a > b)    # Greater than
+print("a <= b:", a <= b)  # Less than or equal to
+print("a >= b:" ,a >= b)  #Greater than or equal to 
